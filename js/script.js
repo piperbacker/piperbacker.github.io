870db1b112317a5a3702c88caad12a409c3280a3
piperbacker <piper.backer@gmail.com>
2024-02-28 00:30:04 +0100

    cleaning up enlarge image event listeners

--- js/script.js
+++ js/script.js
@@ -1,34 +1,33 @@
 const images = ["IMG_0941.jpg", "IMG_0952.jpg", "IMG_0943.jpg",
     "IMG_0937.jpg", "IMG_0949.jpg", "IMG_0954.jpg"];
 
+function enlargeImage(src, img, bg) {
+    document.body.style.overflow = "hidden";
+    img.src = src;
+    img.style.display = "block";
+    bg.style.display = "block";
+}
+
+function closeEnlargedImage(img, bg) {
+    document.body.style.overflow = "auto";
+    img.style.display = "none";
+    bg.style.display = "none";
+}
+
 function displayIllustrations() {
     const imgLarge = document.getElementById('img-large');
     const imgLargeBG = document.getElementById('img-large-bg');
-    imgLargeBG.addEventListener('click', function () {
-        document.body.style.overflow = "auto";
-        imgLarge.style.display = "none";
-        imgLargeBG.style.display = "none";
-    });
+    imgLargeBG.addEventListener('click', function () { closeEnlargedImage(imgLarge, imgLargeBG) });
 
     const grid = document.getElementById('illustrations-grid');
     for (image of images) {
-        const div = document.createElement('div');
+        const imgDiv = document.createElement('div');
         const img = document.createElement('img');
         img.src = "img/illustrations/" + image;
-
         img.role = "button";
-        img.addEventListener('click', function () {
-            document.body.style.overflow = "hidden";
-            const imgLarge = document.getElementById('img-large');
-            const imgLargeBG = document.getElementById('img-large-bg');
-            imgLarge.src = this.src;
-            imgLarge.style.display = "block";
-            imgLargeBG.style.display = "block";
-            console.log("image click", this.src);
-        });
-
-        grid.appendChild(div);
-        div.appendChild(img);
+        img.addEventListener('click', function () { enlargeImage(this.src, imgLarge, imgLargeBG) });
+        grid.appendChild(imgDiv);
+        imgDiv.appendChild(img);
     }
 }
 
